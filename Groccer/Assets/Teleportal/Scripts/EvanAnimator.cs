81af426ee09286355c0225b6fdab14d79d33c38c
nkanthasamy3 <46797844+nkanthasamy3@users.noreply.github.com>
2019-10-27 00:17:36 -0400

    Collisions

---
+++ Groccer/Assets/Teleportal/Scripts/EvanAnimator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Teleportal;
+using System.Threading.Tasks;
+using System;
+
+public class EvanAnimator : MonoBehaviour
+{
+    public TPUser tpuser;
+
+    void Start()
+    {
+        if (this.tpuser.isSelfUser) {
+            this.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0.0f, (0.01f * Mathf.Sin(Time.time * 4)), 0.0f);
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+
+        if (c.gameObject.name == "Sale1")
+        {
+            Debug.Log("Collision detected");
+        }
+
+
+    }
+
+
+    void OnCollisionExit(Collision c)
+    {
+        //this.DestroyLaser();
+    }
+}
---
+++ Groccer/Assets/Teleportal/Scripts/TpCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+
+namespace Assets.Teleportal.Scripts
+{
+    class TpCollision : MonoBehaviour
+    {
+        public bool enter = true;
+        void Update()
+        {
+            
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("WTF");
+            //if (other.gameObject.name == "Sale1")
+            //{
+            //    Debug.Log("Collision detected");
+            //}
+
+        }
+
+
+    }
+    
+}
