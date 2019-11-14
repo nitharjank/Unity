12b405c8906eb80767573b7f46e1cf7ab4591f9d
nkanthasamy3 <46797844+nkanthasamy3@users.noreply.github.com>
2019-11-14 13:00:32 -0500

    Entire files

--- Groccer/Assets/Teleportal/Scripts/EvanAnimator.cs
+++ Groccer/Assets/Teleportal/Scripts/EvanAnimator.cs
@@ -14,14 +14,14 @@ public class EvanAnimator : MonoBehaviour
         if (this.tpuser.isSelfUser) {
             this.enabled = false;
         }
-        Teleportal.GeoModule.Shared.Latitude = 0.0;
-        Teleportal.GeoModule.Shared.Longitude = 0.0;
+        //Teleportal.GeoModule.Shared.Latitude = 0.0;
+        //Teleportal.GeoModule.Shared.Longitude = 0.0;
     }
 
     void Update()
     {
-        transform.position += new Vector3(transform.forward.x, (transform.forward.z), 0.0f);
-        //transform.position += new Vector3(0.0f, (0.01f * Mathf.Sin(Time.time * 4)), 0.0f);
+        //transform.position += new Vector3(transform.forward.x, (transform.forward.y), 0.0f);
+        transform.position += new Vector3(0.0f, (0.01f * Mathf.Sin(Time.time * 4)), 0.0f);
         //transform.position += transform.forward.x;
         //transform.position += TPObject.GetEarthTransform();
     }
